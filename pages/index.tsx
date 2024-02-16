65cc1369455a7dd0907ded28a3ffb897548531be
Qiqian <qichaiqian02@gmail.com>
2024-02-16 17:05:55 +0800

    second commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,15 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { FaApple, FaGooglePlay, FaBluetooth } from "react-icons/fa";
+import { GiMedicines } from "react-icons/gi";
+import { FaDownload } from "react-icons/fa6";
+import { SlCalender } from "react-icons/sl";
+import { IoIosNotifications } from "react-icons/io";
+import { AiOutlineStock } from "react-icons/ai";
+import { IoDocumentText } from "react-icons/io5";
+import { MdMotionPhotosAuto } from "react-icons/md";
+import { GoGoal } from "react-icons/go";
 
 const Home: NextPage = () => {
   return (
@@ -42,14 +51,29 @@ const Home: NextPage = () => {
 
         <section id='device'>
           <div className='flex items-center justify-center mt-[5rem]'>
-            {/* Device and App Photo */}
-            <div className='border-2 border-yellow-500 w-[25rem] h-[25rem] text-center items-center justify-center flex'>Device Photo</div>
+            {/* App Photo */}
+            <div>
+              <div className='border-2 border-yellow-500 w-[25rem] h-[25rem] text-center items-center justify-center flex'>Phone Photo</div>
+              <div className='text-center text-2xl mt-7 p-4 rounded-xl bg-gray-500'>The Application</div>
+            </div>
             {/* Introduce */}
             <div className='ml-5 grid grid-cols-2 gap-3'>
-              <div className='bg-gray-200 w-60 h-60 text-center'>a</div>
-              <div className='bg-gray-200 w-60 h-60 text-center'>a</div>
-              <div className='bg-gray-200 w-60 h-60 text-center'>a</div>
-              <div className='bg-gray-200 w-60 h-60 text-center'>a</div>
+              <div className='bg-gray-200 w-60 h-60 text-center rounded-xl flex flex-col items-center justify-center'>
+                <IoIosNotifications className='text-5xl mb-2' />
+                <p className='text-lg'>Notification / Reminder</p>
+              </div>
+              <div className='bg-gray-200 w-60 h-60 text-center rounded-xl flex flex-col justify-center items-center'>
+                <AiOutlineStock className='text-5xl mb-2' />
+                <p className='text-lg'>View Stock Quantity</p>
+              </div>
+              <div className='bg-gray-200 w-60 h-60 text-center rounded-xl flex flex-col justify-center items-center'>
+                <IoDocumentText className='text-5xl mb-2' />
+                <p className='text-lg'>View Adherence Report</p>
+              </div>
+              <div className='bg-gray-200 w-60 h-60 text-center rounded-xl flex flex-col justify-center items-center'>
+                <SlCalender className='text-5xl mb-2' />
+                <p className='text-lg'>View Calendar</p>
+              </div>
             </div>
           </div>
 
@@ -59,13 +83,60 @@ const Home: NextPage = () => {
           <div className='flex items-center justify-center mt-[5rem]'>
             {/* Introduce */}
             <div className='grid grid-cols-2 gap-3'>
-              <div className='bg-gray-200 w-60 h-60 text-center'>a</div>
-              <div className='bg-gray-200 w-60 h-60 text-center'>a</div>
-              <div className='bg-gray-200 w-60 h-60 text-center'>a</div>
-              <div className='bg-gray-200 w-60 h-60 text-center'>a</div>
+              <div className='px-2 bg-gray-200 w-60 h-60 text-center rounded-xl flex flex-col items-center justify-center'>
+                <IoIosNotifications className='text-5xl mb-2' />
+                <p className='text-lg'>Fit up to 6 various <br /> blister pack</p>
+              </div>
+              <div className='px-2 bg-gray-200 w-60 h-60 text-center rounded-xl flex flex-col items-center justify-center'>
+                <MdMotionPhotosAuto className='text-5xl mb-2' />
+                <p className='text-lg'>Fully automated</p>
+              </div>
+              <div className='px-2 bg-gray-200 w-60 h-60 text-center rounded-xl flex flex-col items-center justify-center'>
+                <GoGoal className='text-5xl mb-2' />
+                <p className='text-lg'>Accurate punching system for precise dispensing</p>
+              </div>
+              <div className='px-2 bg-gray-200 w-60 h-60 text-center rounded-xl flex flex-col items-center justify-center'>
+                <FaBluetooth className='text-5xl mb-2' />
+                <p className='text-lg'>Integrated Bluetooth with mobile app</p>
+              </div>
             </div>
-            {/* Device and App Photo */}
-            <div className=' ml-5 border-2 border-yellow-500 w-[25rem] h-[25rem] text-center items-center justify-center flex'>Phone Photo</div>
+            {/* DevicePhoto */}
+            <div className='ml-5'>
+              <div className='border-2 border-yellow-500 w-[25rem] h-[25rem] text-center items-center justify-center flex'>Device Photo</div>
+              <div className='text-center text-2xl mt-7 p-4 rounded-xl bg-gray-500'>The Device</div>
+            </div>
+          </div>
+        </section>
+
+        <section id='step'>
+          <div className='items-center mt-[5rem]'>
+            <p className='text-2xl text-center'> <strong>How MedPop Works</strong> <br />From Delivery to Dosage </p>
+            <div className="flex justify-center items-center space-x-12 mt-5">
+              <div className="relative w-[14rem] h-[14rem] bg-gray-200 border-2 border-black rounded-full">
+                <div className="absolute inset-0 flex flex-col justify-center items-center">
+                  <GiMedicines className='text-5xl mb-5' />
+                  <p className='text-center'>Place the blister pack <br /> to the device</p>
+                </div>
+              </div>
+              <div className="relative w-[14rem] h-[14rem] bg-gray-200 border-2 border-black rounded-full">
+                <div className="absolute inset-0 flex flex-col justify-center items-center">
+                  <FaDownload className='text-5xl mb-5' />
+                  <p className='text-center'>Download the application <br /> from App Store</p>
+                </div>
+              </div>
+              <div className="relative w-[14rem] h-[14rem] bg-gray-200 border-2 border-black rounded-full">
+                <div className="absolute inset-0 flex flex-col justify-center items-center">
+                  <FaBluetooth className='text-5xl mb-5' />
+                  <p className='text-center'>Connect the device with the App via Bluetooth</p>
+                </div>
+              </div>
+              <div className="relative w-[14rem] h-[14rem] bg-gray-200 border-2 border-black rounded-full">
+                <div className="absolute inset-0 flex flex-col justify-center items-center">
+                  <SlCalender className='text-5xl mb-5' />
+                  <p className='text-center'>Setup your medication schedule</p>
+                </div>
+              </div>
+            </div>
           </div>
         </section>
 
@@ -74,8 +145,14 @@ const Home: NextPage = () => {
             <div>
               <p className='text-xl font-bold text-center'>Transform your health effortlessly. <br /> Dive into MedPop now!</p>
               <div className='flex space-x-5 mt-6 items-center justify-center'>
-                <a className='bg-gray-200 border-2 border-black px-6 py-2 rounded-md hover:scale-[105%] transition-transform' href=''>Learn More</a>
-                <a className='bg-gray-200 border-2 border-black px-6 py-2 rounded-md hover:scale-[105%] transition-transform' href=''>Get It Now</a>
+                <div className='flex items-center bg-gray-200 border-2 w-[13rem] border-black py-2 rounded-xl hover:scale-[105%] transition-transform' >
+                  <FaApple className='text-4xl ml-3 justify-center' />
+                  <p className='ml-2'>Download on the <br /><strong>Apple Store</strong></p>
+                </div>
+                <div className='flex items-center bg-gray-200 border-2 w-[13rem] border-black py-2 rounded-xl hover:scale-[105%] transition-transform' >
+                  <FaGooglePlay className='text-4xl ml-3 justify-center' />
+                  <p className='text-center  w-[9rem]'>Get it on <br /><strong>Google Play</strong></p>
+                </div>
               </div>
             </div>
           </div>

0f102bc8b192f8e9c5b36956f886a114066e2320
zheng0422 <zhengchoo0422@gmail.com>
2024-02-21 17:04:38 +0800

    v1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+      "3xl": "1920px",
+      QHD: "2560px",
+      "4K": "3840px",
+    },
     extend: {},
   },
   plugins: [],
